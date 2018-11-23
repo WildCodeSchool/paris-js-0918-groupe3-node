d5cba8a47170debc5f0d0f8ab5179c41b09ca2f1
ChristopheRibault <c.ribault.dev@gmail.com>
2018-11-23 16:32:26 +0100

    first route and dump.sql

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//imports
 const express = require('express');
 const app = express();
 const connection = require('./config');
@@ -6,13 +7,26 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
+// use
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//routes
 
+app.get('/api/questions', (req, res) => {
+  connection.query('SELECT text, is_custom FROM questions WHERE is_custom=false', (err, results) => {
+    if (err) {
+      res.status(500).send(`Erreur serveur : ${err}`);
+    } else {
+      res.json(results);
+    }
+  })
+})
 
+//launch
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Connection impossible');
