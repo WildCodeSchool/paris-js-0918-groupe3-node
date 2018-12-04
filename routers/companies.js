a6a421b957fa8b5daf124ec1a33f6289e10f2e40
ChristopheRibault <c.ribault.dev@gmail.com>
2018-12-04 15:08:27 +0100

    refac routes

--- routers/companies.js
+++ routers/companies.js
@@ -2,36 +2,37 @@
 const express = require('express');
 const connection = require('../config');
 const router = express.Router();
-const today = new Date();
 
-router.get('/:id_companies', (req, res) => {
-  const sql = `
-    SELECT name, siret, description, logo, link, email
-    FROM companies
-    WHERE is_active
-    AND id=?`;
-  connection.query(sql, req.params.id_companies, (err, results) => {
-    if (err) {
-      res.status(500).send(`Erreur serveur : ${err}`)
-    } else {
-      res.send(results)
-    }
-  });
-});
+router.route('/:id_companies')
 
-router.put('/:id_companies', (req, res) => {
-  const dataForm = req.body;
-  const sql = `
-  UPDATE companies 
-  SET ?
-  WHERE id = ?`;
-  connection.query(sql, [dataForm, req.params.id_companies], (err, results) => {
-    if (err){
-      res.status(500).send(`Erreur serveur : ${err}`)
-    } else {
-      res.send(req.body)
-    }
+  .get((req, res) => {
+    const sql = `
+      SELECT name, siret, description, logo, link, email
+      FROM companies
+      WHERE is_active
+      AND id=?`;
+    connection.query(sql, req.params.id_companies, (err, results) => {
+      if (err) {
+        res.status(500).send(`Erreur serveur : ${err}`)
+      } else {
+        res.send(results)
+      }
+    });
+  })
+
+  .put('/:id_companies', (req, res) => {
+    const dataForm = req.body;
+    const sql = `
+    UPDATE companies 
+    SET ?
+    WHERE id = ?`;
+    connection.query(sql, [dataForm, req.params.id_companies], (err, results) => {
+      if (err){
+        res.status(500).send(`Erreur serveur : ${err}`)
+      } else {
+        res.send(req.body)
+      }
+    });
   });
-});
 
 module.exports = router;
