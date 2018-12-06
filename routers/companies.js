46e6fa51bca0d8907742ad258740bf1126715f3b
FabienTS <f.thalabas@gmail.com>
2018-12-06 19:21:40 +0100

    rajout du POST new company et GET alias POST pour recuper  id, à reparer

--- routers/companies.js
+++ routers/companies.js
@@ -3,6 +3,45 @@ const express = require('express');
 const connection = require('../config');
 const router = express.Router();
 
+router.route('/')
+  .post((req, res) => {
+    const sql = `SELECT id FROM companies WHERE email = ${req.body.email}`;
+    connection.query(sql, (err, results) => {
+      if (err) {
+        res.status(500).send(`Erreur serveur : ${err}`)
+      } else {
+        res.send(results[0])
+      }
+    });
+  })
+
+  // .post((req, res) => {
+  //   fixData = {
+  //     id: null,
+  //     siret: 123456789,
+  //     description: 'test',
+  //     logo: 'test.png',
+  //     link: 'test',
+  //     created_at: new Date(),
+  //     updated_at: new Date(),
+  //     password: 'testtest',
+  //     is_active: true
+  //   };
+
+  //   const formData = Object.assign(fixData, req.body);
+  //   const sql = 'INSERT INTO companies SET ? ';
+  //   connection.query(sql, formData, (err, results) => {
+  //     if (err) {
+  //       res.status(500).send(`Erreur serveur : ${err}`)
+  //     } else {
+  //       res.json(results)
+  //     }
+  //   })
+  // })
+
+
+
+
 router.route('/:id_companies')
 
   .get((req, res) => {
@@ -27,7 +66,7 @@ router.route('/:id_companies')
     SET ?
     WHERE id = ?`;
     connection.query(sql, [dataForm, req.params.id_companies], (err, results) => {
-      if (err){
+      if (err) {
         res.status(500).send(`Erreur serveur : ${err}`)
       } else {
         res.send(req.body)
