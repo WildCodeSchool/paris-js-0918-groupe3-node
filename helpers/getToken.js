3b96d691355b5da357d469b87bab6ec4db52a27e
D0C702WH0 <jemyplu@gmail.com>
2019-01-08 09:52:14 +0100

    autHHHHHorization

--- helpers/getToken.js
+++ helpers/getToken.js
@@ -1,8 +1,8 @@
 const getToken = req => {
-    if (req.headers.autorization &&
-        req.headers.autorization.split(' ')[0] === 'Bearer'
+    if (req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
         ){
-            return req.headers.autorization.split(' ')[1];
+            return req.headers.authorization.split(' ')[1];
         }
     return null;
 }
