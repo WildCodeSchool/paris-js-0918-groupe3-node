f775743ba75fad377cc2e1db398a4dc8a1b16ffd
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-10 09:04:24 +0100

    routes put applications

--- routers/applications.js
+++ routers/applications.js
@@ -4,6 +4,7 @@ const connection = require("../config");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const jwtSecret = require("../secure/jwtSecret");
+const getToken = require('../helpers/getToken');
 
 /**
  * Allows to post answers for questions of an offer
@@ -13,7 +14,6 @@ const jwtSecret = require("../secure/jwtSecret");
 router.post("/answer", (req, res) => {
   const token = getToken(req);
   jwt.verify(token, jwtSecret, (err, decode) => {
-   
     if (!err) {
       const dataForm = {
         ...req.body,
@@ -37,5 +37,68 @@ router.post("/answer", (req, res) => {
 });
 
 
+router.route('/send')
+  .put((req, res) => {
+    const token = getToken(req);
+    const {
+      id_candidates,
+      id_offers,
+    } = req.body;
+    const dataForm = {
+      is_sent: 1,
+    };
+    jwt.verify(token, jwtSecret, (err, decode) => {
+      if (!err && decode.id === Number(id_candidates) && decode.role === 'candidates') {
+        const sql = `UPDATE applications SET ? WHERE id_candidates = ? AND id_offers = ?`;
+        connection.query(sql, [dataForm, id_candidates, id_offers], (err, results) => {
+          if (err) res.sendStatus(500);
+          else res.status(201).send(results);
+        });
+      } else {
+        res.sendStatus(403);
+      }
+    });
+  });
+
+router.route('/status')
+  .put((req, res) => {
+    const token = getToken(req);
+    const {
+      id_candidates,
+      id_offers,
+      status
+    } = req.body;
+    if (status !== 'validated' && status !== 'rejected') {
+      res.status(400).send('ERR : Status not valid please choose "validated" or "rejected"')
+    } else {
+      const dataForm = {
+        status
+      };
+      // Check company token
+      jwt.verify(token, jwtSecret, (err, decode) => {
+        if (err) res.sendStatus(403);
+        else {
+          const sqlGetIDComp = `SELECT id_companies FROM offers WHERE id= ?`;
+          connection.query(sqlGetIDComp, id_offers, (err, results) => {
+            if (err) res.sendStatus(500)
+            else {
+              // Checks if token id corresponds to the company the offers belongs to
+              if (results[0].id_companies === decode.id) {
+                const sql = `UPDATE applications SET ? WHERE id_candidates = ? AND id_offers = ?`;
+                connection.query(sql, [dataForm, id_candidates, id_offers], (err, results) => {
+                  if (err) res.sendStatus(500)
+                  else {
+                    res.status(201).send(results);
+                  }
+                });
+              } else {
+                res.sendStatus(403);
+              }
+            }
+          });
+        }
+      });
+    }
+  });
 
 module.exports = router;
