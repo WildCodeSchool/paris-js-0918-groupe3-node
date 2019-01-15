8127656827225afea6ae4b23adee41f14c6000e3
D0C702WH0 <jemyplu@gmail.com>
2019-01-15 14:37:26 +0100

    email token ok for company + refacto

---
+++ helpers/emailToken.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const password = process.env.GMAIL_PASS;
+
+module.exports = (to, url) => {
+  let smtpTransporter = nodemailer.createTransport({
+    service: `gmail`,
+    host: "smtp.gmail.com",
+    auth: {
+      user: `jemyplu@gmail.com`,
+      pass: password
+    }
+  });
+
+  let mailOptions = {
+    from: "jeremy <jemyplu@gmail.com>", // sender address
+    to: to, // list of receivers
+    subject: "Validation inscription", // Subject line
+    html: `<p>Veuillez confirmer votre adresse mail en cliquant sur le lien suivant : <a href="${url}">CLIQUEZ ICI</a> </p>`
+  };
+  smtpTransporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("message envoyé");
+
+    return response;
+  });
+};
--- routers/auth.js
+++ routers/auth.js
@@ -9,6 +9,7 @@ const connection = require("../config");
 const jwtSecret = require("../secure/jwtSecret");
 const router = express.Router();
 const getFileExtension = require("../helpers/getFileExtension");
+const emailToken = require("../helpers/emailToken");
 
 const emailRegex = require("../secure/emailRegex");
 
@@ -87,64 +88,109 @@ router
       if (crypErr) res.sendStatus(500);
       if (!emailRegex.test(req.body.email))
         res.status(403).send("email non valide");
-      if (!req.file) {
+      else {
+        const newPath = req.file
+          ? "public/logoCompanies/" +
+            `logo_${req.body.name
+              .trim()
+              .replace(" ", "_")}_${Date.now()}.${getFileExtension(
+              req.file.mimetype
+            )}`
+          : "";
+
+        if (req.file) {
+          fs.rename(req.file.path, newPath, err => {
+            if (err) res.status(500).send(err);
+          });
+        }
+
         const dataForm = {
           name: req.body.name,
           siret: req.body.siret,
-          logo: req.body.logo,
           link: req.body.link,
           email: req.body.email,
           description: req.body.description,
           id: null,
           created_at: new Date(),
           updated_at: new Date(),
-          is_active: 1,
-          password: hash
+          is_active: 0,
+          password: hash,
+          logo: newPath
         };
         const sql = `INSERT INTO companies SET ?`;
         connection.query(sql, dataForm, (err, results) => {
+          console.log("VA DANS INSERT", sql);
+
           if (err) res.status(200).send({ error: err });
           else {
-            res.json({ id: results.insertId });
-          }
-        });
-      } else {
-        const newPath =
-          "public/logoCompanies/" +
-          `logo_${req.body.name
-            .trim()
-            .replace(" ", "_")}_${Date.now()}.${getFileExtension(
-            req.file.mimetype
-          )}`;
-
-        fs.rename(req.file.path, newPath, err => {
-          if (err) res.status(500).send(err);
-          else {
-            const dataForm = {
-              name: req.body.name,
-              siret: req.body.siret,
-              link: req.body.link,
-              email: req.body.email,
-              description: req.body.description,
-              id: null,
-              created_at: new Date(),
-              updated_at: new Date(),
-              is_active: 1,
-              password: hash,
-              logo: newPath
+            const tokenInfo = {
+              id: results.insertId,
+              userType: "companies",
+              expiresIn: "1d"
             };
-            const sql = `INSERT INTO companies SET ?`;
-            connection.query(sql, dataForm, (err, results) => {
-              if (err) res.status(200).send({ error: err });
-              else {
-                res.json({ id: results.insertId });
-              }
+            jwt.sign(tokenInfo, jwtSecret, (err, Token) => {
+              console.log("VA DANS TOKEN", Token);
+
+              const to = req.body.email;
+              const url = `http://localhost:3002/api/auth/confirmation/${Token}`;
+              if (!err) emailToken(to, url);
             });
+
+            res.json({ id: results.insertId });
           }
         });
       }
     });
   });
+
+router.get("/confirmation/:emailToken", (req, res) => {
+  const token = req.params.emailToken;
+  try {
+    console.log("lalilulelo", token);
+    jwt.verify(token, jwtSecret, async (error, decode) => {
+      if (!error) {
+        const userType = decode.userType;
+        const id = decode.id;
+        const sql = `UPDATE ${userType} SET is_active=1  WHERE id=${id}`;
+        await connection.query(sql, (err, results) => {
+          console.log("RESULST", results);
+          console.log("SQL", sql);
+
+          if (err) results.status(403).send({ error: err });
+          else {
+            console.log("ALL RIGHT");
+          }
+        });
+      }
+    });
+  } catch (e) {
+    res.send("error");
+  }
+  return res.json({ validation: "votre demande à bien été prise en compte" });
+});
+
+// .get("/confirmation/:emailToken", async (req, res) => {
+//   try {
+//     const data = { is_active: 1 };
+//     const {
+//       id: { id }
+//     } = jwt.verify(req.params.token, jwtSecret);
+//     const {
+//       userType: { userType }
+//     } = jwt.verify(req.params.token, jwtSecret);
+//     const sql = `UPDATE ${userType} SET ? WHERE id =${id}`;
+//     await connection.query(sql, data),
+//       (err, results) => {
+//         if (err) results.status(403).send({ error: err });
+//         else {
+//           results.sendStatus(200);
+//         }
+//       };
+//   } catch (e) {
+//     res.send("error");
+//   }
+//   return res.json({ is_active: 1 });
+// });
 
 router.route("/signup/candidates").post((req, res) => {
   bcrypt.hash(req.body.password, 10, (crypErr, hash) => {
