9a4724c2bb0bed4555606b023cd102958d8e7bdd
ChristopheRibault <c.ribault.dev@gmail.com>
2019-01-17 17:21:24 +0100

    router questions

---
+++ ajv/schemas/postQuestion.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "maxProperties" : 1,
+  "minProperties" : 1,
+  "required" : ["text"],
+  "properties": {
+    "text" : {
+      "type": "string",
+    },
+  },
+};
--- routers/questions.js
+++ routers/questions.js
@@ -1,59 +1,52 @@
 /**** imports *****/
 const express = require('express');
-const connection = require('../config');
+const knex = require('../dbconfig');
+
+/**** data Validation *****/
+
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const postQuestionDataIsValid = ajv.compile(require('../ajv/schemas/postQuestion'));
+
 const router = express.Router();
+
 
 /**
  * Sends original questions only.
  */
-router.get('/originals', (req, res) => {
-  const sql = `SELECT id, text FROM questions WHERE is_custom=false`;
-  connection.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).send(`Erreur serveur : ${err}`);
-    } else {
-      res.json(results);
-    }
-  })
-})
+router.get('/originals', async (req, res) => {
+  const results = await knex.select('id', 'text').from('questions').where({is_custom:0});
+  res.json(results);
+});
 
 /**
  * Sends the text of the question from its id
  */
-router.get('/:id_question', (req,res) => {
-  const sql = `SELECT text FROM questions WHERE id= ?`;
-  connection.query(sql, req.params.id_question, (err, results) => {
-    if (err) {
-      res.status(500).send(`Erreur serveur : ${err}`)
-    }else{
-      res.json(results[0])
-    }
-  });
+router.get('/:id_question', async (req,res) => {
+  const { id_question } = req.params;
+  const result = await knex.select('text').from('questions').where({id:id_question});
+  res.json(result[0]);
 });
 
 /**
  * Posts a new custom question and sends the id and the text of the question.
  */
-router.post('/', (req, res) => {
-  const formData = {
-    id: null,
-    text: req.body.text,
-    is_custom: true,
-    created_at: new Date(),
-    updated_at: new Date()
-  }
-  const sql = `INSERT INTO questions SET ?`;
-
-  connection.query(sql, formData, (err, results) => {
-    if (err) {
-      res.status(500).send(`Erreur serveur : ${err}`);
-    } else {
-      res.json({
-        id: results.insertId,
-        text: formData.text
-      });
+router.post('/', async (req, res) => {
+  if(!postQuestionDataIsValid(req.body)) res.status(400).send('données non valides');
+  else {
+    const formData = {
+      id: null,
+      text: req.body.text,
+      is_custom: 1,
+      created_at: new Date(),
+      updated_at: new Date()
     }
-  })
+    const results = await knex('questions').insert(formData);
+    res.status(201).json({
+      id: results[0],
+      text: formData.text
+    });
+  }
 });
 
 module.exports = router;
